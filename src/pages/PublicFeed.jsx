3df8c351775edc1398b622a2887a003beb2c2694
ee-rpeach <157056610+ee-rpeach@users.noreply.github.com>
2025-07-08 22:20:58 -0400

    added some lines in Public Feed, won't authorize?

--- src/pages/PublicFeed.jsx
+++ src/pages/PublicFeed.jsx
@@ -1,7 +1,36 @@
+import {useEffect, useState} from "react";
+import { Link } from "react-router-dom";
+
+
 export default function PublicFeed(){
+    const [trips, setTrips] = useState([])
+    
+    useEffect(()=>{
+        const fetchTrips = async () => {
+        try {
+            const res = await fetch("http://localhost:3000/api/trip/public");
+            const data = await res.json();
+            setTrips(data);
+        } catch (err) {
+            console.error(err)
+        }
+        }
+        fetchTrips();
+    }, []);
 
     return(
         <>
+            <div className="publicTripGrid">
+                {trips.map((trip) => (
+
+                            
+            <Link to={`/api/trip/${trip.id}`}>
+            <div key={trip.id} className="tripCard">
+                <h3 className="tripTitle">{trip.title}</h3>
+            </div>
+            </Link>
+            ))}  
+            </div>
         </>
     )
 }
