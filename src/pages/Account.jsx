31ba83beabbfd9d7c627a54ea205d1232d012b88
ee-rpeach <157056610+ee-rpeach@users.noreply.github.com>
2025-07-15 21:40:28 -0400

    got public feed working

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -3,18 +3,21 @@ import { Link } from "react-router-dom";
 
 
 export default function Account(){
-    const [user, setUser] = useState([])
+    const [user, setUser] = useState({})
     const token = localStorage.getItem("token")
     
     useEffect(()=>{
         const fetchTrips = async () => {
         try {
             const res = await fetch("http://localhost:3000/api/user/me",
-            {headers: {Authorization: `Bearer ${token}`
-                }})
+            {
+                headers: {Authorization: `Bearer ${token}`}
+            })
             const data = await res.json();
+            console.log(`data = ${data}`)
             console.log(data)
             setUser(data);
+            console.log(`user = ${user}`)
         } catch (err) {
             console.error(err)
         }
@@ -24,10 +27,14 @@ export default function Account(){
 
     return(
         <>
-        <h1>Account Information</h1>        
-        <div className="userDetails">
-            <h3 className="Username">{user.user.name}</h3>
-        </div>
+        <h1>Account Information</h1>
+        {user ?         
+            <div className="userDetails">
+                <h3 className="Username">{user.name}</h3>
+            </div>
+        :
+            <></>
+        }
         </>
     )
 }
