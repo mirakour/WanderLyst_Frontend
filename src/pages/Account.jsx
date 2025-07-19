a57eaaa6392bc2aff96542106e7a13d3efcf525b
ee-rpeach <157056610+ee-rpeach@users.noreply.github.com>
2025-07-19 14:32:03 -0400

    Events component on details page

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -32,6 +32,9 @@ export default function Account( {token, setUserId } ){
                 <div className="userDetails">
                 <h3 className="Username">{user.name}</h3>
                 </div>
+            <h2>Upcoming Trips</h2>
+            
+            <h2>Past Trips</h2>
         <MyTrips token={token}/>
             </div>
             :
