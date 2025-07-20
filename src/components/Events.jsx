4ccde1ea0cc29a8e73efe6e56c5fcc82da82c391
KalebMorton <kmorton7714@gmail.com>
2025-07-20 14:57:01 -0600

    made the "Create New Event" form it's own seperate component

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,15 +1,11 @@
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
+import EventCreationFrom from "./EventCreationFrom";
 
 export default function Events(passedData){
     const [message, setMessage] = useState(null)
     const [eventsList, setEventsList] = useState([]) //this varible holds a list of all the events
-
     const [showForm, setShowForm] = useState(false)
-    const [title, setTitle] = useState(null)
-    const [location, setLocation] = useState(null)
-    const [date, setDate] = useState(null)
-    const [status, setStatus] = useState(null)
    
     async function getEvents() {
         try{
@@ -38,72 +34,16 @@ export default function Events(passedData){
        getEvents() 
     },[showForm]);
 
-    async function handleSubmit(event){
-        event.preventDefault();
-
-        try{
-            const response = await fetch(`http://localhost:3000/api/trip/${passedData.tripId}/events`,{
-                method: "POST", 
-                headers: { 
-                    "Content-Type": "application/json",
-                    "Authorization":  `Bearer ${passedData.token}`
-                }, 
-                body: JSON.stringify({ 
-                    title: title, 
-                    location: location,
-                    date_time: date,
-                    status: status
-                })
-            })
-            const rawData = await response.json()
-            setMessage(rawData.message)
-            if(rawData.message == "event created"){
-                setShowForm(false)
-            }
-        }catch (error){
-            console.error(error)
-        }
-    }
-
     function handleFormButton(event){
         event.preventDefault()
         setMessage(null)
         setShowForm(true)
     }
-
-    function handleBackButton(){
-        event.preventDefault()
-        setShowForm(false)
-    }
     
     return(
     <>
         {showForm ? 
-            <div id="formBox">
-                <h2>Create Event</h2>
-                {message && <div>{message}</div>}
-                <form onSubmit={handleSubmit}>
-                    <label id="formContent">
-                        Event Title: <input value={title} onChange={(e) => setTitle(e.target.value)}/>
-                    </label>
-                    <br></br>
-                    <label id="formContent">
-                        Event Location: <input value={location} onChange={(e) => setLocation(e.target.value)}/>
-                    </label>
-                    <br></br>
-                    <label id="formContent">
-                        Event Date: <input type="date" value={date} onChange={(e) => setDate(e.target.value)}/>
-                    </label>
-                    <br></br>
-                    <label id="formContent">
-                        Event Status: <input value={status} onChange={(e) => setStatus(e.target.value)}/>
-                    </label>
-                    <br></br>
-                    <button id="formContent">Submit</button>
-                    <button onClick={handleBackButton}>Cancel</button> 
-                </form>
-                
-            </div>
+            <EventCreationFrom token={passedData.token} tripId={passedData.tripId} setShowForm={setShowForm}/>
             : 
             eventsList.length > 0 ? 
                 <div>
